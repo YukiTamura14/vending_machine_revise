cf8d946a5b836460e739b42d8adc0fabf500900a
Hiroyoshi NORO <noro@diveintocode.jp>
2019-07-28 14:30:25 +0200

    Add () to instance initialized.

--- lib/drink.rb
+++ lib/drink.rb
@@ -2,14 +2,14 @@ class Drink
   attr_reader :name, :price
 
   def self.milk
-    self.new 100, :milk
+    self.new(100, :milk)
   end
 
   def self.water
-    self.new 0, :water
+    self.new(0, :water)
   end
 
-  def initialize price, name
+  def initialize(price, name)
     @name = name
     @price = price
   end
